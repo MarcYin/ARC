174b2aaaab7516d712df756e5250bfe63e95fa6b
Feng Yin <marcollinbobo@gmail.com>
2023-06-22 12:49:25 +0100

    new setup

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='ARC',
-    version='0.0.1',
-    author='Feng Yin',
-    author_email='ucfafyi@ucl.ac.uk',
-    description='ARC python package',
-    packages=find_packages(),
-    license = "GNU Affero General Public License v3.0",
-    include_package_data = True,
-
-)
+from setuptools import setup
+if __name__ == '__main__':
+    setup()
